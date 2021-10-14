4b22001031763c1e29a41dc1d927e83a5a4094ee
Karl Andersson <kaan1552@gullstrand.it.uu.se>
2021-10-14 16:36:17 +0200

    Added files for MA4 and plot

--- MA4_files/MA4_2.py
+++ MA4_files/MA4_2.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+from time import perf_counter as pc
+from time import sleep as pause
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 def main():
 	f = Integer(5)
 	print(f.get())
+	print(f.fib())
 	f.set(7)
 	print(f.get())
+	print(f.fib())
 	print(fib_py(5))
-	
 
 def fib_py(n):
 	if n <= 1:
@@ -17,4 +25,21 @@ def fib_py(n):
 		return(fib_py(n-1)+fib_py(n-2))
 
 if __name__ == '__main__':
+
+	n=np.arange(1,20)
+	tid_cpp=[]
+	tid_py=[]
+	for i in range(1,20):
+		start = pc()
+		fib_py(i)
+		end = pc()
+		tid_py.append(end-start)
+		f=Integer(i)
+		start_cpp=pc()
+		f.fib()
+		end_cpp=pc()
+		tid_cpp.append(end-start)
+	plt.plot(n,tid_py,'bo')
+	plt.plot(n,tid_cpp,'ro')
+	plt.savefig('MA4_testplot.png')
 	main()
--- MA4_files/integer.cpp
+++ MA4_files/integer.cpp
@@ -5,9 +5,11 @@ class Integer{
 	public:
 		Integer(int);
 		int get();
+		int fib();
 		void set(int);
 	private:
 		int val;
+		int fibH(int);
 	};
  
 Integer::Integer(int n){
@@ -17,7 +19,17 @@ Integer::Integer(int n){
 int Integer::get(){
 	return val;
 	}
- 
+int Integer::fib(){
+	return(fibH(val));}
+int Integer::fibH(int n){
+	if((n==1)||(n==0)){
+	return(n);}
+	else{
+	return(fibH(n-1)+fibH(n-2));
+	}
+} 
+
+
 void Integer::set(int n){
 	val = n;
 	}
@@ -26,6 +38,7 @@ void Integer::set(int n){
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
+	int Integer_fib(Integer* integer) {return integer->fib();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
